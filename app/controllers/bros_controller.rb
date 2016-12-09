84d447f4985a1cce1b0a1b05b9d3e7a172379a37
Jonathan Pham <jon.pham@gmail.com>
2016-12-08 20:19:53 -0800

    Add other Routes For API Usage.

--- app/controllers/bros_controller.rb
+++ app/controllers/bros_controller.rb
@@ -7,4 +7,22 @@ class BrosController < ApplicationController
   def show
     @bro = Bro.find_by(id: params[:id])
   end
+
+  def create
+    @bro = Bro.create({name: params[:name],bio: params[:bio],fav_food: params[:fav_food], job: params[:job]})
+    render :show
+  end
+
+  def update
+    @bro = Bro.find_by(id: params[:id])
+    @bro.assign_attributes({name: params[:name],bio: params[:bio],fav_food: params[:fav_food], job: params[:job]})
+    @bro.save
+    render :show
+  end
+
+  def destroy
+    @bro = Bro.find_by(id: params[:id])
+    @bro.destroy
+    redirect_to :index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/bros', to: 'bros#index'
   get '/bros/:id', to: 'bros#show'
+  post '/bros', to: 'bros#create'
+  patch '/bros/:id', to: 'bros#update'
+  delete 'bros/:id', to: 'bros#destroy'
 end
