73a2639af27d1ff816c56d55b3ea5b4f6a95df1a
Jonathan Pham <jon.pham@gmail.com>
2016-12-15 18:27:35 -0800

    Add Email & Header Token Check before Authorization.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,13 @@ class ApplicationController < ActionController::Base
   before_action :restrict_access
   
   def restrict_access
-    authenticate_or_request_with_http_token do |api_key,options|
-      true
+    authenticate_or_request_with_http_token do |token,options|
+      # raise
+      User.find_by(api_key: token,email: response.headers['X-User-Email'])
+      #   return true
+      # else
+      #   return false
+      # end
     end
   end
 end
