6cdd370f4735c801cc80facf937135b17dfd5244
Jonathan Pham <jon.pham@gmail.com>
2016-12-15 18:18:00 -0800

    Add User Model and BCrypt to store secure password.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161209051918) do
+ActiveRecord::Schema.define(version: 20161216021308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,16 @@ ActiveRecord::Schema.define(version: 20161209051918) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "bro_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "api_key"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
